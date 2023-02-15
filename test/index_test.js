77291630f59bcb46f355e60fdc1b6e243659bb47
Vraj Desai <43074241+vrajdesai78@users.noreply.github.com>
2023-02-15 19:40:00 +0530

    External Adapter created

---
+++ test/index_test.js
@@ -0,0 +1,49 @@
+const assert = require('chai').assert
+const createRequest = require('../index.js').createRequest
+
+describe('createRequest', () => {
+  const jobID = '1'
+
+  context('successful calls', () => {
+    const requests = [
+     { name: 'id not supplied', testData: { data: { city: 'boston' } } },
+      { name: 'city', testData: { id: jobID, data: { city: 'boston' } } },
+      { name: 'q', testData: { id: jobID, data: { q: 'boston' } } },
+      { name: 'town', testData: { id: jobID, data: { town: 'boston' } } }
+    ]
+
+    requests.forEach(req => {
+      it(`${req.name}`, (done) => {
+        createRequest(req.testData, (statusCode, data) => {
+          assert.equal(statusCode, 200)
+          assert.equal(data.jobRunID, jobID)
+          assert.isNotEmpty(data.data)
+          assert.isAbove(Number(data.result), 0)
+          assert.isAbove(Number(data.data.result), 0)
+          done()
+        })
+      })
+    })
+  })
+
+  context('error calls', () => {
+    const requests = [
+     { name: 'empty body', testData: {} },
+      { name: 'empty data', testData: { data: {} } },
+      { name: 'city not supplied', testData: { id: jobID, data: { dog: 'boston' } } },
+      { name: 'unknown city', testData: { id: jobID, data: { city: 'not_real'} } },
+    ]
+
+    requests.forEach(req => {
+      it(`${req.name}`, (done) => {
+        createRequest(req.testData, (statusCode, data) => {
+          assert.equal(statusCode, 500)
+          assert.equal(data.jobRunID, jobID)
+          assert.equal(data.status, 'errored')
+          assert.isNotEmpty(data.error)
+          done()
+        })
+      })
+    })
+  })
+})
